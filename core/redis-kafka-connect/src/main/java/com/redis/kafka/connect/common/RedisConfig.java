1be01bf0daeaa5ef92e1130074afb316e65da085
Saurav Kumar <saukumar@confluent.io>
2025-10-25 07:43:32 +0530

    changing service-name key

--- core/redis-kafka-connect/src/main/java/com/redis/kafka/connect/common/RedisConfig.java
+++ core/redis-kafka-connect/src/main/java/com/redis/kafka/connect/common/RedisConfig.java
@@ -146,7 +146,7 @@ public abstract class RedisConfig extends AbstractConfig {
         configs.put("rediskafka.provider.username",
             configs.get("redis.username"));
         configs.put("rediskafka.provider.service.name",
-            toLowerCase(configs.get("rediskafka.credentials.provider.aws.cluster.service.name")));
+            toLowerCase(configs.get("rediskafka.cluster.service.name")));
         configs.put("aws.credentials.expiration.duration.secs", "900");
 
         logger.info("RedisCredentialsProvider, configs: {}", configs);
